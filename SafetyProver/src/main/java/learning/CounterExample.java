e54129e1bb3fc5971d680279e0ebfbcc4d3c1edc
ericpony <ericpony@csie.org>
2017-01-25 16:50:20 +0800

    record counterexamples

--- SafetyProver/src/main/java/learning/CounterExample.java
+++ SafetyProver/src/main/java/learning/CounterExample.java
@@ -1,21 +1,37 @@
 package learning;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CounterExample {
     private List<Integer> cex;
     private boolean isPositive;
+    private Set<List<Integer>> history = null;
+
+    public CounterExample() {
+        this(true);
+    }
+
+    public CounterExample(boolean recordCexs) {
+        if (recordCexs)
+            history = new HashSet<List<Integer>>(100);
+    }
 
     public void addNegative(List<Integer> ex) {
-        if (cex != null) throw new IllegalStateException("cex is already set");
+        if (cex != null) throw new IllegalStateException("counterexample is set twice!");
         cex = ex;
+        if (history != null && !history.add(cex))
+            throw new IllegalStateException("teacher produced duplicate counterexamples!");
         isPositive = false;
     }
 
     public void addPositive(List<Integer> ex) {
-        if (cex != null) throw new IllegalStateException("cex is already set");
+        if (cex != null) throw new IllegalStateException("counterexample is set twice!");
         cex = ex;
+        if (history != null && !history.add(cex))
+            throw new IllegalStateException("teacher produced duplicate counterexamples!");
         isPositive = true;
     }
 
